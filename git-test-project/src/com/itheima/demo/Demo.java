9cb45ca62bf3a8631f724f2f300885ec2eb279ff
bxzr11 <53325598+bxzr11@users.noreply.github.com>
2019-08-09 17:23:48 +0800

    idea测试提交变量i

---
+++ git-test-project/src/com/itheima/demo/Demo.java
@@ -0,0 +1,20 @@
+package com.itheima.demo;
+
+/**
+ * Created by 欧Sir on 2019/8/9.
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Git上传测试");
+        System.out.println("Git上传测试");
+        System.out.println("Git上传测试");
+        System.out.println("Git上传测试");
+    }
+
+    public void test(){
+        int i=0;
+        i+=10;
+        System.out.println("测试方法"+i);
+    }
+}
