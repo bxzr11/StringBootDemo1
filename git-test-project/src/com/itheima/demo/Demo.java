c1e137b25602905e8dd7ee3aff5ad5f6c3342226
bxzr11 <53325598+bxzr11@users.noreply.github.com>
2019-08-09 17:35:37 +0800

    clone上传测试

--- git-test-project/src/com/itheima/demo/Demo.java
+++ git-test-project/src/com/itheima/demo/Demo.java
@@ -10,11 +10,13 @@ public class Demo {
         System.out.println("Git上传测试");
         System.out.println("Git上传测试");
         System.out.println("Git上传测试");
+        //歐Sirs的git上传测试
     }
 
     public void test(){
         int i=0;
         i+=10;
         System.out.println("测试方法"+i);
+        System.out.println("上传更新");
     }
 }
