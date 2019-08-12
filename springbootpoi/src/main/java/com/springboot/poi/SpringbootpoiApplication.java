837a3df46f4bb5a37f1eba578acfa9d55146716e
bxzr11 <53325598+bxzr11@users.noreply.github.com>
2019-08-12 10:10:07 +0800

    创建Springbootpoi的Demo项目

---
+++ springbootpoi/src/main/java/com/springboot/poi/SpringbootpoiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.poi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootpoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootpoiApplication.class, args);
+	}
+
+}
